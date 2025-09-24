d920ce6c85f08d5baf787baca9174562f992fb4d
Grizzlishe78 <grizzlishe78@gmail.com>
2025-09-24 23:28:25 +0700

    Tugas 3 Pertemuan 4

--- app/src/main/java/com/unsoed/informatikamobile/data/model/SearchResponse.kt
+++ app/src/main/java/com/unsoed/informatikamobile/data/model/SearchResponse.kt
@@ -1,3 +1,17 @@
 package com.unsoed.informatikamobile.data.model
 
-data class SearchResponse()
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName( value = "docs")
+    val docs: List<BookDoc>
+)
+
+data class BookDoc(
+    @SerializedName(value = "title")
+    val title: String,
+    @SerializedName(value = "author_name")
+    val authorName: List<String>?,
+    @SerializedName(value = "first_publish_year")
+    val firstPublishYear: Int?
+)
